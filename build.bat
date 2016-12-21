e402747a702b0b0309d7190e27e67f14ec8c88dd
mohamed el aqqad <mohamed.el-aqqad@outlook.com>
2016-12-21 19:17:08 +0100

    changed first build file

--- build.bat
+++ build.bat
@@ -1,3 +1,2 @@
 dotnet restore "SoaWebsite.Common\project.json"  "SoaWebsite.Services\project.json"  "SoaWebsite.Web\project.json"
 dotnet build "SoaWebsite.Common\project.json"  "SoaWebsite.Services\project.json"  "SoaWebsite.Web\project.json"
-dotnet migrate "SoaWebsite.Common\project.json"  "SoaWebsite.Services\project.json"  "SoaWebsite.Web\project.json"
