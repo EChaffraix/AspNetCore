33ca0201aeec23b43eff62d64ad73832f08526dd
mohamed el aqqad <mohamed.el-aqqad@outlook.com>
2016-12-21 19:12:28 +0100

    create a visual studio project by converting .json files into xproj files

---
+++ build.bat
@@ -0,0 +1,3 @@
+dotnet restore "SoaWebsite.Common\project.json"  "SoaWebsite.Services\project.json"  "SoaWebsite.Web\project.json"
+dotnet build "SoaWebsite.Common\project.json"  "SoaWebsite.Services\project.json"  "SoaWebsite.Web\project.json"
+dotnet migrate "SoaWebsite.Common\project.json"  "SoaWebsite.Services\project.json"  "SoaWebsite.Web\project.json"
